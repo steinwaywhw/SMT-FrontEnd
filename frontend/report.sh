05f13b312447ce6964db9042c7e48c69b78fa84c
Steinway Wu <steinwaywhw@gmail.com>
2013-05-04 12:29:00 -0400

    Revise

--- frontend/report.sh
+++ frontend/report.sh
@@ -2,23 +2,30 @@
 
 declare -a LOGICS=("QF_IDL" "QF_NIA" "QF_UFBV" "QF_UFLIA" "UFNIA" "QF_BV" "QF_LIA" "QF_UF" "QF_UFIDL")
 
-
 for LOGIC in "${LOGICS[@]}"
-do
-	echo "$LOGIC"
+do	
 	COUNT_ALL=$(grep "^$LOGIC" benchmark.report | wc -l)
 	COUNT_UNSAT=$(grep "^$LOGIC unsat" benchmark.report | wc -l)
 	COUNT_SAT=$(grep "^$LOGIC sat" benchmark.report | wc -l)
-	COUNT_CVC_CORRECT_SAT=$(grep -E "^$LOGIC sat (cvc4|both)" benchmark.report | wc -l)
+
 	COUNT_CVC_CORRECT_UNSAT=$(grep -E "^$LOGIC unsat (cvc4|both)" benchmark.report | wc -l)
-	COUNT_ALT_CORRECT=$(grep -E "^$LOGIC (unsat|sat) (alt\-ergo|both)" benchmark.report | wc -l)
+	COUNT_CVC_ERROR_UNSAT=$(grep -E "^$LOGIC unsat .+ .+ (sat|error)" benchmark.report | wc -l)
+	COUNT_CVC_TIMEOUT_UNSAT=$(grep -E "^$LOGIC unsat .+ .+ timeout" benchmark.report | wc -l)
+	COUNT_CVC_UNKNOWN_UNSAT=$(grep -E "^$LOGIC unsat .+ .+ unknown" benchmark.report | wc -l)
 
-	#grep -E "^QF_IDL unsat \w* \w* timeout" benchmark.report |
+	COUNT_ALT_CORRECT=$(grep -E "^$LOGIC unsat (alt\-ergo|both)" benchmark.report | wc -l)
+	COUNT_ALT_ERROR=$(grep -E "^$LOGIC unsat .+ .+ .+ .+ (sat|error)" benchmark.report | wc -l)
+	COUNT_ALT_TIMEOUT=$(grep -E "^$LOGIC unsat .+ .+ .+ .+ timeout" benchmark.report | wc -l)
+	COUNT_ALT_UNKNOWN=$(grep -E "^$LOGIC unsat .+ .+ .+ .+ unknown" benchmark.report | wc -l)
 
+	COUNT_CVC_CORRECT_SAT=$(grep -E "^$LOGIC sat (cvc4|both)" benchmark.report | wc -l)
+	COUNT_CVC_TIMEOUT_SAT=$(grep -E "^$LOGIC sat .+ .+ timeout" benchmark.report | wc -l)
+	COUNT_CVC_ERROR_SAT=$(grep -E "^$LOGIC sat .+ .+ error" benchmark.report | wc -l)
+	COUNT_CVC_UNKNOWN_SAT=$(grep -E "^$LOGIC sat .+ .+ unknown" benchmark.report | wc -l)
 
 	COUNT=0
 	CVC_TIME_SAT=0
-	ALL_TIMES=$(awk "/^$LOGIC sat (cvc4|both)/ {print \$6}" benchmark.report)
+	ALL_TIMES=$(awk "/^$LOGIC sat .+ .+ sat/ {print \$6}" benchmark.report)
 	for TIME in $ALL_TIMES
 	do
 		let COUNT+=1
@@ -30,7 +37,7 @@ do
 
 	COUNT=0
 	CVC_TIME_UNSAT=0
-	ALL_TIMES=$(awk "/^$LOGIC unsat \w+ \w+ unsat/ {print \$6}" benchmark.report)
+	ALL_TIMES=$(awk "/^$LOGIC unsat .+ .+ unsat/ {print \$6}" benchmark.report)
 	for TIME in $ALL_TIMES
 	do
 		let COUNT+=1
@@ -42,7 +49,7 @@ do
 
 	COUNT=0
 	ALT_TIME_UNSAT=0
-	ALL_TIMES=$(awk "/^$LOGIC unsat \w+ \w+ \w+ .+ unsat/ {print \$8}" benchmark.report)
+	ALL_TIMES=$(awk "/^$LOGIC unsat .+ .+ .+ .+ unsat/ {print \$8}" benchmark.report)
 	for TIME in $ALL_TIMES
 	do
 		let COUNT+=1
@@ -52,16 +59,5 @@ do
 		ALT_TIME_UNSAT=$(echo "scale=2; $ALT_TIME_UNSAT / $COUNT" | bc -q)
 	fi 
 
-
-	#done
-	echo "all    $COUNT_ALL"
-	echo "unsat  $COUNT_UNSAT"
-	echo "sat    $COUNT_SAT"
-	echo "cvc_s  $COUNT_CVC_CORRECT_SAT"
-	echo "cvc_n  $COUNT_CVC_CORRECT_UNSAT"
-	echo "alt    $COUNT_ALT_CORRECT"
-	echo "cvc_ts $CVC_TIME_SAT"
-	echo "cvc_tn $CVC_TIME_UNSAT"
-	echo "alt_tn $ALT_TIME_UNSAT"
-	echo ""
+	printf "%s unsat(%d) cvc(%d,%d,%d,%d) alt(%d,%d,%d,%d) cvc(%.2f) alt(%.2f)\n" "$LOGIC" "$COUNT_UNSAT" "$COUNT_CVC_CORRECT_UNSAT" "$COUNT_CVC_TIMEOUT_UNSAT" "$COUNT_CVC_ERROR_UNSAT" "$COUNT_CVC_UNKNOWN_UNSAT" "$COUNT_ALT_CORRECT" "$COUNT_ALT_TIMEOUT" "$COUNT_ALT_ERROR" "$COUNT_ALT_UNKNOWN" "$CVC_TIME_UNSAT" "$ALT_TIME_UNSAT"
 done
